bf3b5b160cb6dc76be284d2e6a1a21621f79cb1b
TsarTechSoftware <root@ip-172-31-29-188.us-east-2.compute.internal>
2018-04-24 05:32:48 +0000

    Updated to fulfill requirments

--- js/slider.js
+++ js/slider.js
@@ -18,5 +18,5 @@ function showSlides() {
     }
     slides[showSlides.slideIndex-1].style.display = "block";  
     dots[showSlides.slideIndex-1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
+    setTimeout(showSlides, 1000); // Change image every 2 seconds
 }
